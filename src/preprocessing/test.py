f890eaea2a9fe5f78d86afdb971b1d5d850c49a0
Subin Maharjan <subinmaharjan01@gmail.com>
2026-04-04 20:04:27 +1000

    Initial project setup for data preprocessing and feature engineering

---
+++ src/preprocessing/test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("Environment setup successful")
